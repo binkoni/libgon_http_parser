4ddaf4d9ae958dcf143864891516ed39f664bae7
Byeonggon Lee <gonny952@gmail.com>
2017-09-10 19:11:07 +0900

    Commit

--- test/gon_http_parser_test.c
+++ test/gon_http_parser_test.c
@@ -69,7 +69,6 @@ void gon_http_parser_test(void** state) {
     while((dirent = readdir(directory)) != NULL) {
         if(strncmp("input", dirent->d_name, sizeof("input") - 1) == 0) {
             int inputFd = open(dirent->d_name, O_RDONLY);
-            //warnx("opened %s", dirent->d_name);
             assert_int_not_equal(inputFd, -1);
             int readSize;
             while((readSize = read(inputFd, gon_http_parser_getBufferPosition(parser), gon_http_parser_getAvailableBufferSize(parser))) > 0) {
@@ -91,46 +90,3 @@ int main() {
     const struct CMUnitTest tests[] = {cmocka_unit_test(gon_http_parser_test)};
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
-
-/*
-    while((readSize = TUCUBE_LOCAL_CLIENT_IO->methods->read(
-              TUCUBE_LOCAL_CLIENT_IO,
-              gon_http_parser_getBufferPosition(TUCUBE_LOCAL_PARSER),
-              gon_http_parser_getAvailableBufferSize(TUCUBE_LOCAL_PARSER)
-           )) > 0) {
-        int result;
-        if((result = gon_http_parser_parse(TUCUBE_LOCAL_PARSER, readSize, (void*[]){GENC_LIST_ELEMENT_NEXT(module), GENC_LIST_ELEMENT_NEXT(clData)})) == -1) {
-            warnx("%s: %u: Parser error", __FILE__, __LINE__);
-            return -1;
-        } else if(result == 0)
-            break;
-    }
-    if(readSize == -1) {
-        if(errno == EAGAIN) {
-//            warnx("%s: %u: Client socket EAGAIN", __FILE__, __LINE__);
-            return 1;
-        } else if(errno == EWOULDBLOCK) {
-            warnx("%s: %u: Client socket EWOULDBLOCK", __FILE__, __LINE__);
-            return 1;
-        } else
-            warn("%s: %u", __FILE__, __LINE__);
-        return -1;
-    }
-    else if(readSize == 0) {
-        warnx("%s: %u: Client socket has been closed", __FILE__, __LINE__);
-        return -1;
-    }
-    
-    const char* connectionHeaderValue;
-    if(TUCUBE_LOCAL_MODULE->onGetRequestStringHeader(
-              GENC_LIST_ELEMENT_NEXT(module), GENC_LIST_ELEMENT_NEXT(clData), "Connection", &connectionHeaderValue
-      ) != -1) {
-        if(strncasecmp(connectionHeaderValue, "Keep-Alive", sizeof("Keep-Alive")) == 0) {
-            warnx("%s: %u: Keep-Alive Connection", __FILE__, __LINE__);
-            TUCUBE_LOCAL_CLDATA->isKeepAlive = false;
-            gon_http_parser_reset(TUCUBE_LOCAL_PARSER);
-            return 2; // request finished but this is keep-alive
-        }
-        TUCUBE_LOCAL_CLDATA->isKeepAlive = false;
-    }
-*/
